662a20f9a7e413379dd7971494408eb81892d819
AhmadStar <sqlrhaf@gmail.com>
2022-10-20 10:01:04 +0300

    lang

--- .history/platform/themes/martfury/partials/short-codes/sliders.blade_20220504063847.php
+++ .history/platform/themes/martfury/partials/short-codes/sliders.blade_20220504063847.php
@@ -30,7 +30,7 @@
                         @endforeach
                     </div>
                 </div>
-                {{-- <div class="ps-section__right">
+                <div class="ps-section__right">
                     @if (count($ads))
                         @foreach ($ads as $key)
                             <div class="ps-collection">
@@ -49,7 +49,7 @@
                             </div>
                         @endif
                     @endif
-                </div> --}}
+                </div>
             @else
                 <div class="ps-carousel--nav-inside owl-slider" data-owl-auto="true" data-owl-loop="true" data-owl-speed="5000" data-owl-gap="0" data-owl-nav="true" data-owl-dots="true" data-owl-item="1" data-owl-item-xs="1" data-owl-item-sm="1" data-owl-item-md="1" data-owl-item-lg="1" data-owl-duration="1000" data-owl-mousedrag="on" data-owl-animate-in="fadeIn" data-owl-animate-out="fadeOut">
                     @foreach($sliders as $slider)
